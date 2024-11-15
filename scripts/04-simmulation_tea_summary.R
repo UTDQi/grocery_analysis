797826e1e51fe211a68c5f551a533a6f09bbf1ea
sakura hu <sakura.hu@mail.utoronto.ca>
2024-11-14 21:30:08 -0500

    updated simulation and test for tea_summary

--- scripts/04-simmulation_tea_summary.R
+++ scripts/04-simmulation_tea_summary.R
@@ -21,19 +21,21 @@ n <- 100
 tea_summary <- read_csv("~/grocery_new/data/02-analysis_data/tea_summary.csv")
 product_names <- unique(tea_summary$product_name)
 vendors <- unique(tea_summary$vendor)
-brands <- unique(tea_summary$brand[!is.na(tea_summary$brand)])
 
 # Simulating the data
 simulated_data <- tibble(
   vendor = sample(vendors, size = n, replace = TRUE),  # Random vendors
   product_name = sample(product_names, size = n, replace = TRUE),  # Random product names from the list
-  brand = sample(brands, size = n, replace = TRUE),  # Random brand from the list
   min_price = runif(n, min = 1, max = 20),  # Random prices between 1 and 20
   max_price = min_price + runif(n, min = 0, max = 10),  # Ensure max_price is at least min_price
-  stock = sample(100:500, size = n, replace = TRUE)  # Random stock count
+  days_with_price_data = sample(100:500, size = n, replace = TRUE)  # Random stock count
 )
+
+# Adding the average price column
+simulated_data <- simulated_data %>%
+  mutate(avg_price = (min_price + max_price) / 2)
 
 # View the simulated data
 head(simulated_data)
 
-write_csv(simulated_data, file = "data/simmulated_data/simulated.csv")
+write_csv(simulated_data, file = "data/00-simulated_data/simulated_data.csv")
