d40bcd3e5ab598f33e21257b1c4020fcb826ee98
UTDQi <113342231+UTDQi@users.noreply.github.com>
2024-11-14 20:45:48 -0500

    Added back SQL to create fig 2(price of lipton ginseng lemon tea for different vendors)

---
+++ scripts/05-analysis_price_change.sql
@@ -0,0 +1,22 @@
+/*
+#### Preamble ####
+Purpose: Outline the change in price of lipton ginseng lemon green tea
+Author: David Qi
+Date: 14 Nov 2024
+Contact: haobo.ren@mail.utoronto.ca
+License: MIT
+Pre-requisites: NA
+Any other information needed? Make sure you are in the `starter_folder` rproj
+*/
+
+
+
+
+SELECT nowtime, vendor, product_id, product_name, brand, current_price, old_price, units, price_per_unit, other, brand, units
+	FROM raw 
+	INNER JOIN product 
+	ON raw.product_id = product.id 
+	WHERE
+		(product.product_name LIKE '%green tea%' OR product.product_name LIKE '%greentea%')
+		AND product.brand LIKE '%lipton%' AND product.product_name LIKE '%ginseng%' AND product.product_name LIKE '%Lemon%'
+		And raw.current_price IS NOT NULL;
